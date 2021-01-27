86d271f7a70f8dfbd6b8992de724b0c8be3cd07a
Walaa Alomari <walaahmad1987@gmail.com>
2021-01-27 13:49:39 +0200

    =add js file

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var Q = prompt ( 'do you like fruit salad ' , 'y/n');
+
+if (Q = 'y'){alert ('GREAT !!')};
+
+else if (Q= 'n'){ alert ('i invit you to try it ')};
+
+else if (Q != 'n'and  Q != 'y '){alert ('welcome ')};
+
+
+
+var userName = prompt ('type your name please ','name');
+alert (userName) ;
+console.log (userName);
+
