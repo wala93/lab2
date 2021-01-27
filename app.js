a7f176d81c3cdce8cdf0e9f31a8c11a38df2cf87
Walaa Alomari <walaahmad1987@gmail.com>
2021-01-27 14:47:34 +0200

    solve coding error

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 
-var Q = prompt ( 'do you like fruit salad ' , 'y/n');
+var Q = prompt ( 'do you like fruit salad y/n');
 
-if (Q = 'y'){alert ('GREAT !!')};
+if (Q == 'y'){alert ('GREAT !!')}
 
-else if (Q= 'n'){ alert ('i invit you to try it ')};
+else if (Q== 'n'){ alert ('i invit you to try it ')}
 
-else if (Q != 'n', Q != 'y '){alert ('welcome ')};
+else   {alert ('welcome ')}
 
 
 
 var userName = prompt ('type your name please ','name');
-alert ( 'Im happy to help you , '  userName) ;
+Document.write ('WELCOME ' + userName);
+alert  ( 'Im happy to help you') ;
 
 alert ( ' <img src = "https://e7.pngegg.com/pngimages/891/663/png-clipart-blushing-emoji-emoticon-smiley-emoji-face-embarrassment.png">');
